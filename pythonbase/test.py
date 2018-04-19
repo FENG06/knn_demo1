afe10ce17f09164e9d2c42fbafad9d5c853b4b1e
Kevin <sevenny@foxmail.com>
2018-04-19 10:18:16 +0800

    Python

--- pythonbase/test.py
+++ pythonbase/test.py
@@ -1,5 +1,13 @@
-import time
+import heapq
 import os
+import queue
+import random
+import time
+from collections import deque
+
+import myQueue
+import Stack
+import types
 
 a_list = ['a', 'b', 'mpilgram', 'z', 'example']
 a_list1 = list((3, 5, 7, 8, 11))
@@ -231,3 +239,177 @@ for key, value in aDict.items():
 
 print(aDict.keys())
 print(aDict.values())
+
+aDict['age'] = 38
+print(aDict)
+
+aDict['address'] = 'SDIBT'
+print(aDict)
+
+print(aDict.items())
+aDict.update({'a': 'a', 'b': 'b'})
+print(aDict)
+
+a = {3, 5}
+
+a.add(7)
+print(a)
+
+a_set = set(range(8, 14))
+print(a_set)
+
+data = range(10)
+print(list(data))
+
+print(random.choice(data))
+print(random.choice(data))
+# random.shuffle(data)
+# print(data)
+
+heap = []  # 建堆
+for n in data:
+    heapq.heappush(heap, n)
+print(heap)
+
+heapq.heappush(heap, 0.5)
+print(heap)
+
+print(heapq.heappop(heap))
+print(heapq.heappop(heap))
+print(heapq.heappop(heap))
+
+print("------------分割---------------")
+
+myheap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 4, 10, 333]
+print(myheap)
+print(heapq.heapreplace(myheap, 6))
+print(myheap)
+
+print("--------Queue模块的使用方法-----------")
+# q = Queue.Queue()
+# q.put(0)
+# q.put(1)
+# q.put(2)
+#
+# print(q.queue)
+
+q = queue.Queue()
+q.put(0)
+q.put(1)
+q.put(2)
+print(q.queue)
+print(q.get())
+print(q.queue)
+
+queue = deque(["Eric", "John", "Michael"])
+queue.append("Terry")
+queue.append("Graham")
+print(queue.popleft())
+print(queue.popleft())
+print(queue)
+
+q = myQueue.myQueue()
+q.get()
+q.put(5)
+q.show()
+q.put(7)
+q.put('a')
+q.show()
+q.get()
+q.get()
+q.get()
+q.get()
+
+print("----------栈的使用--------------")
+myStack = []
+myStack.append(3)
+myStack.append(5)
+myStack.append(7)
+print(myStack)
+print(myStack.pop())
+print(myStack.pop())
+print(myStack.pop())
+# print(myStack.pop())
+
+s = Stack.Stack()
+s.push(3)
+s.show()
+s.push(5)
+s.show()
+s.push(7)
+s.show()
+s.pop()
+s.show()
+
+print('----------------二叉树的使用------------')
+
+# import BinaryTree
+
+# root = BinaryTree.BinaryTree('root')
+
+
+print('-----------运算符与条件表达式------------')
+
+
+def Join(chList, sep=None):
+    return (sep or ',').join(chList)
+
+
+chTest = ['1', '2', '3', '4', '5']
+print(Join(chTest))
+print(Join(chTest, ':'))
+print(Join(chTest, " "))
+
+print('----------------选择结构的使用------------')
+
+# endFlag = 'yes'
+# s = 0
+#
+# while endFlag.lower() == 'yes':
+#     x1 = eval(input("请输入一个正整数："))
+#     print(type(x1))
+#     if type(x1) == int and 0 <= x1 <= 100:
+#         s = s + x1
+#     else:
+#         print('不是数字或不符合要求')
+#     endFlag = input('继续输入?(yes or no):')
+# print('整数之和=', s)
+
+
+print('-------------循环结构的使用-------------')
+
+import time
+import math
+
+# start = time.time()
+# for i in range(10000000):
+#     math.sin(i)
+# print('Time Used:', time.time() - start)
+# loc_sin = math.sin
+# start = time.time()
+# for i in range(10000000):
+#     loc_sin(i)
+# print('Time Used:', time.time() - start)
+#
+# print('-----循环结构使用及导入方式的改进-------')
+# import time
+# from math import sin as sin
+#
+# start = time.time()
+# for i in range(10000000):
+#     sin(i)
+# print('Time Used:', time.time() - start)
+# loc_sin = sin
+# start = time.time()
+# for i in range(10000000):
+#     loc_sin(i)
+# print('Time Used:', time.time() - start)
+#
+# print('---------break 和 continue的使用----------')
+
+i = 1
+while i < 10:
+    if i % 2 == 0:
+        continue
+    print(i)
+    i += 1
