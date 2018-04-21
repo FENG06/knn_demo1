29eea0a8469772006f314c87897dd5f9f98db7cc
Kevin <sevenny@foxmail.com>
2018-04-21 19:56:42 +0800

    Python类的使用
    特殊成员定义和访问的方法

--- pythonbase/Class.py
+++ pythonbase/Class.py
@@ -35,3 +35,26 @@ class A:
 a = A()
 print(a._value1)
 print(a._A__value2)  # 在外部访问对象的私有数据成员
+
+print('----------特殊成员定义和访问的方法---------')
+
+
+class Fruit:
+    def __init__(self):
+        self.__color = 'Red'
+        self.price = 1
+
+
+apple = Fruit()
+print(apple.price)  # 显示对象公开数据成员的值
+
+apple.price = 2  # 修改对象公开数据成员的值
+print(apple.price)
+print(apple._Fruit__color)  # 显示对象私有成员
+
+apple._Fruit__color = "Blue"  # 修改对象私有成员变量的值
+
+print(apple.price)
+print(apple._Fruit__color)  # 显示对象私有成员
+
+# print(apple.__color)  # 不能直接访问对象的 私有数据成员，出错
